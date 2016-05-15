d7f9472b0b2e8df89a2728cf465b1ff50707a4e4
Iru Cai <mytbk920423@gmail.com>
2016-05-15 22:33:10 +0800

    sign with card and verify
    not working yet...

--- CCIDPkg/ccidtest.c
+++ CCIDPkg/ccidtest.c
@@ -1,5 +1,5 @@
 #include "ccid_oper.h"
-#include <cryptodata.h>
+#include <openpgp/cryptodata.h>
 #include <Library/UefiApplicationEntryPoint.h>
 #include <Protocol/SimpleFileSystem.h>
 #include <Guid/FileInfo.h>
@@ -19,6 +19,10 @@ static unsigned char *keyid = NULL;
 static CHAR16* pubkeyfile = L"xxxxxxxx.gpg";
 static unsigned char pubkeydata[8192];
 static UINTN pubkeybuflen;
+static struct RSA_pubkey rsakey;
+static unsigned char sigmsgbuf[1024];
+static unsigned int sigbuflen=0;
+static UINTN readlen=0;
 
 static void
 targetu16fn(CHAR16* fn)
@@ -144,16 +148,47 @@ UefiMain(
 		Print(L"\n");
 		keyid = sigs+16;
 		targetu16fn(pubkeyfile);
+		Print(pubkeyfile);
+		Print(L"\n");
 
 		EFI_FILE_PROTOCOL *Root;
 		EFI_FILE_PROTOCOL *pubFileHandle;
+		pubkeybuflen = sizeof(pubkeydata);
 		SAFECALLE(Status, GetFileIo(&Root));
 		SAFECALLE(Status, Root->Open(Root, &pubFileHandle, pubkeyfile, EFI_FILE_MODE_READ, 0));
 		SAFECALLE(Status, pubFileHandle->Read(pubFileHandle, &pubkeybuflen, pubkeydata));
 		SAFECALLE(Status, pubFileHandle->Close(pubFileHandle));
+		AsciiPrint("%d bytes read from public key file.\n", (int)pubkeybuflen);
 
-		SAFECALLE(Status, PGP_VerifyPW1(ccid, (const unsigned char*)"TestOnly", 8));
+		if (find_pubkey(pubkeydata, pubkeybuflen, &rsakey, sigs+12)!=0) {
+			AsciiPrint("Cannot find the public key of the OpenPGP card.\n");
+			return EFI_ABORTED;
+		}
+
+		SAFECALLE(Status, PGP_VerifyPW1(ccid, (const unsigned char*)"testOnly", 8));
 		SAFECALLE(Status, PGP_Sign(ccid, sha256digestinfo, SHA256_DIGEST_INFO_LEN));
+
+		do {
+			readlen = 1024;
+			SAFECALLE(Status, RecvData(ccid, sigmsgbuf, &readlen));
+		} while (readlen==0);
+		sigbuflen += readlen-2;
+		if (sigmsgbuf[readlen+1]==0x90 && sigmsgbuf[readlen+2]==0x00) {
+			AsciiPrint("Signature received.\n");
+		} else if (sigmsgbuf[readlen+1]==0x61){
+			UINTN requestlen = sigmsgbuf[readlen+2];
+			UINTN nextlen = requestlen;
+			while (nextlen!=0) {
+				SAFECALLE(Status, PGP_GetResponse(ccid, sigmsgbuf+sigbuflen, requestlen, &nextlen));
+				sigbuflen += requestlen;
+				requestlen = nextlen;
+			}
+		}
+		if (sigverify(sigmsgbuf, sigbuflen, HASH_SHA256, sha256digestinfo+19, &rsakey)==1) {
+			AsciiPrint("verify signature success!\n");
+		} else {
+			AsciiPrint("verify failed.\n");
+		}
 	}
 
 	return EFI_SUCCESS;
